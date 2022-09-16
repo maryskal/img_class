ff56fd3b7abdb31e3adffd53418a0e6877051f67
maryskal_gpus <meroll16@gmail.com>
2022-09-16 19:30:20 -0400

    borrar mask1

--- funciones_imagenes/losses.py
+++ funciones_imagenes/losses.py
@@ -16,24 +16,3 @@ def dice_coef(y_true, y_pred, smooth=100):
 
 def dice_coef_loss(y_true, y_pred):
     return 1 - dice_coef(y_true, y_pred)
-
-
-mask_model = keras.models.load_model('./modelos/mask_1.h5', 
-                                    custom_objects={"dice_coef_loss": dice_coef_loss, "dice_coef": dice_coef})
-sub_mask = keras.Model(inputs=mask_model.input, outputs=mask_model.layers[18].output)
-sub_mask.trainable = False
-
-
-def loss_mask(y_true, y_pred):
-    y_pred = sub_mask(y_pred)
-    y_true = sub_mask(y_true)
-    return 0.6*abs(y_true - y_pred)
-
-
-def MyLoss(y_true, y_pred):
-    # Loss 1
-    loss1 = dice_coef_loss(y_true, y_pred)
-    # Loss 2
-    loss2 = loss_mask(y_true, y_pred)
-    loss = loss1 + loss2
-    return loss
